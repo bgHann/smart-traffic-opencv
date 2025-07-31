c3373bbae3f4f70b939fd7dd08790193300c205d
Han <hanbuii7705@gmail.com>
2025-08-01 01:12:31 +0700

    Thêm bài CV2: vẽ khung + chèn tên

---
+++ cv2_draw_name.py
@@ -0,0 +1,40 @@
+import cv2
+
+# Đọc ảnh
+image = cv2.imread(r"D:\GIAHAN\CN2304CLCA\QTQN-CNTT\car3.jpg")
+if image is None:
+    print("Không tìm thấy ảnh, vui lòng kiểm tra lại")
+    exit()
+
+# Kích thước ảnh
+h, w = image.shape[:2]
+
+# Tính vị trí khung chữ nhật giữa ảnh
+top_left = (w // 4, h // 4)
+bottom_right = (3 * w // 4, 3 * h // 4)
+
+# Vẽ khung màu vàng
+cv2.rectangle(image, top_left, bottom_right, (0, 255, 255), 3)
+
+# Nội dung chữ
+text = "Bui Gia Han"
+
+# Font và cỡ chữ
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 1
+thickness = 2
+
+# Tính kích thước chữ (dùng để canh giữa)
+(text_width, text_height), baseline = cv2.getTextSize(text, font, font_scale, thickness)
+
+# Tính vị trí để chữ nằm giữa khung
+x_text = top_left[0] + (bottom_right[0] - top_left[0] - text_width) // 2
+y_text = top_left[1] + (bottom_right[1] - top_left[1] + text_height) // 2
+
+# Viết chữ màu đỏ
+cv2.putText(image, text, (x_text, y_text), font, font_scale, (0, 0, 255), thickness)
+
+# Hiển thị kết quả
+cv2.imshow("Ảnh kết quả", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
